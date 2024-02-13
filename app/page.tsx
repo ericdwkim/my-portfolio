fa57bb2603e038b4132e4ff55b89063344795203
ericdwkim <ericdwkim.io@gmail.com>
2024-02-13 13:49:34 -0600

    wip: footer.tsx ; add: LinkedIn href to page.tsx for now

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
-import { Header } from '../components/header';
-import { Footer } from '../components/footer';
+import { Header } from '@/components/header';
+// import { Footer } from '../components/footer';
 
 export default function Home() {
   return (
       <section>
         <Header name="Eric Kim"> </Header>
       <p>Welcome to my portfolio </p>
+          <a href={"https://www.linkedin.com/in/ericdwkim/"}>LinkedIn</a>
       </section>
   )
 }
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,7 +1,12 @@
-export function Footer() {
-    return (
-        <footer>
-            <a href="https://www.linkedin.com/in/ericdwkim/">LinkedIn</a>
-        </footer>
-    )
-}
+// import { BsLinkedin } from "react-icons/bs";
+//
+// export function Footer() {
+//     return (
+//         <footer>
+//             <a href="https://www.linkedin.com/in/ericdwkim/">
+//                 <span>LinkedIn</span>{''}
+//                 <BsLinkedin className="hidden sm:block ml-1" />
+//             </a>
+//         </footer>
+//     )
+// }
