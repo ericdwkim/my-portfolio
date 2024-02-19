f75dca95be520cd9a37c7b91d924f7c23219d581
ericdwkim <ericdwkim.io@gmail.com>
2024-02-19 17:18:16 -0600

    wip: hobbies component

---
+++ components/hobbies.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import React from 'react';
+import {SectionHeading} from "@/components/section-heading";
+
+export const Hobbies = () => {
+    return (
+        <div>Hi</div>
+    )
+
+}
+
+
+// todo: add imbedded iframe for chess game
+
+// <iframe id="11525955" allowTransparency="true" frameBorder="0" style="width:100%;border:none;"
+//         src="//www.chess.com/emboard?id=11525955"></iframe>
+// <script>window.addEventListener("message",e=>{e['data'] && "11525955" === e['data']['id'] && document.getElementById(`${e['data']['id']}`) && (document.getElementById(`${e['data']['id']}`).style.height = `${e['data']['frameHeight'] + 30}px`)});</script>
