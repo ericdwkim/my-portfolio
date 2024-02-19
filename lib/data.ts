7d27d66a5071701dbda5750f2b5411cf3226f834
ericdwkim <ericdwkim.io@gmail.com>
2024-02-19 16:34:46 -0600

    add: `links`

---
+++ lib/data.ts
@@ -0,0 +1,36 @@
+import React from 'react';
+import { CgWorkAlt } from "react-icons/cg";
+import { LuGraduationCap} from "react-icons/lu";
+import { FaReact } from 'react-icons/fa';
+
+export const links = [
+    {
+        name: 'Home',
+        hash: '#home',
+    },
+    {
+        name: 'About',
+        hash: '#about',
+    },
+    {
+        name: 'Projects',
+        hash: '#projets',
+    },
+    {
+        name: 'Skills',
+        hash: '#skills',
+    },
+    {
+        name: 'Experience',
+        hash: '#experience',
+    },
+    {
+        name: 'Hobbies',
+        hash: '#hobbies',
+    },
+    {
+        name: 'Contact',
+        hash: '#contact',
+    },
+
+] as const;
