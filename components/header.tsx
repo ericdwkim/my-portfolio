c0180700128506affcd851e605221da9559ed899
ericdwkim <ericdwkim.io@gmail.com>
2024-02-20 16:18:31 -0600

    wip: header component

--- components/header.tsx
+++ components/header.tsx
@@ -1,5 +1,26 @@
+'use client';
+
+import React from 'react';
+import { motion } from 'framer-motion'
+import { links } from '@/lib/data';
+
+import { useActiveSectionContext } from "@/context/active-section-context";
+
 export const Header = () => {
     return (
-        <header>hello! im a header</header>
+        <header className="z-[999] relative">
+            <motion.div
+                className="fixed top-0 left-1/2 h-[4.5rem] w-full rounded-none border border-white border-opacity-45 bg-white bg-opacity-80 shadow-lg shadow-black[0.03] backdrop-blur-[0.5rem] sm:top-6 sm:h[3.25rem] sm:w-[44rem] sm:rounded-full dark:bg-gray-950 dark:border-black/40 dark:bg-opacity-75"
+                initial={{ y: -100, x: '-50%', opacity: 0 }}
+                animate={{ y: 0, x: '-50%', opacity: 1 }}
+            ></motion.div>
+
+
+            <nav>
+
+            </nav>
+
+
+        </header>
     )
 }
