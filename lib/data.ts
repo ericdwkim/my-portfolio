7b1dfdcd560c5dd80107ab763fb2c3f94f448a3f
ericdwkim <ericdwkim.io@gmail.com>
2024-02-28 15:43:38 -0600

    add: `experiencesData` list in data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -1,7 +1,8 @@
 import React from 'react';
-import { CgWorkAlt } from "react-icons/cg";
+// import { CgWorkAlt } from "react-icons/cg";
 import { LuGraduationCap} from "react-icons/lu";
-import { FaReact } from 'react-icons/fa';
+// import { FaReact } from 'react-icons/fa';
+import { BsPersonWorkspace } from "react-icons/bs";
 
 export const links = [
     {
@@ -33,4 +34,41 @@ export const links = [
         hash: '#contact',
     },
 
+] as const;
+
+export const experiencesData = [
+    {
+        title: 'Data Analyst | Aramark',
+        description:
+        'Aramark is an American food and facilities services provider across diverse sectors with international operations. ENABLE+ is a set of iOS/Android apps designed to streamline food servicing and inventory management. Prima Web is a web app designed to manage menu items, recipes, and food waste tracking.',
+        icon: React.createElement(BsPersonWorkspace),
+        date: 'Feb 2022 - Present'
+    },
+    {
+        title: 'Developer / Data Analyst | TXB Stores',
+        description:
+        'placeholder description',
+        icon: React.createElement(BsPersonWorkspace),
+        date: 'Dec 2021 - Feb 2022'
+    },
+    {
+        title: 'Jr. Software Engineer / Technical Analyst | Fruitful Labs',
+        description:
+        'placeholder description',
+        icon: React.createElement(BsPersonWorkspace),
+        date: 'Dec 2020 - Sept 2021'
+    },
+    {
+        title: 'Student | Backend, SQL, and DevOps with Python | NuCamp',
+        description: 'placeholder',
+        icon: React.createElement(LuGraduationCap),
+        date: 'Aug 2021 - Dec 2021'
+    },
+    {
+        title: 'Bachelors of Science | Temple University | Philadelphia, PA',
+        description:
+        'Kinesiology major with a minor in Neuroscience Research',
+        icon: React.createElement(LuGraduationCap),
+        date: 'Dec 2016 - Dec 2020'
+    },
 ] as const;
