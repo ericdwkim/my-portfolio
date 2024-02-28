202b1ee3a7753e8d92307c78678426d3dd3437b1
ericdwkim <ericdwkim.io@gmail.com>
2024-02-28 16:24:08 -0600

    add: `Experience` component to `Home` component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Intro } from '@/components/intro';
 import { About } from '@/components/about';
+import { Experience } from '@/components/experience';
 
 export default function Home() {
     return (
         <main className="flex flex-col items-center px-4 overflow-x-hidden">
             <Intro />
+            <Experience />
             <About />
         </main>
   )
