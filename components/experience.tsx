3e725eac969e00347515f4896a558a185342067e
ericdwkim <ericdwkim.io@gmail.com>
2024-03-01 17:23:49 -0600

    fix: hidden `vertical-timeline-element` elements in dev server

--- components/experience.tsx
+++ components/experience.tsx
@@ -19,6 +19,7 @@ export const Experience = () => {
     const experienceData: any = experiencesData.map((item , index ) =>(
         <React.Fragment key={index}>
             <VerticalTimelineElement
+                visible={true}
                 contentStyle={{
                     background:
                     theme === 'light' ? '#f3f4f6' : 'rgba(255, 255, 255, 0.05)',
