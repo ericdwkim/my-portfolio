f2d900f11d7b8389c7f2c5cc4b23e7623c200d96
ericdwkim <ericdwkim.io@gmail.com>
2024-03-18 14:59:06 -0500

    update: homepage with divided sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,16 @@
 import { Intro } from '@/components/intro';
 import { About } from '@/components/about';
 import { Experience } from '@/components/experience';
+import { SectionDivider } from "@/components/section-divider";
 
 export default function Home() {
     return (
         <main className="flex flex-col items-center px-4 overflow-x-hidden">
             <Intro />
-            <About />
+            <SectionDivider />
             <Experience />
+            <SectionDivider />
+            <About />
         </main>
   )
 }
