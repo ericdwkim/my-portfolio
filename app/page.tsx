cd2767dfea879a4da9455b5d225ae8c8bf965589
ericdwkim <ericdwkim.io@gmail.com>
2024-03-19 15:53:12 -0500

    add: `Skills` component

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,14 @@ import { Contact } from '@/components/contact';
 import { Experience } from '@/components/experience';
 import { Intro } from '@/components/intro';
 import { SectionDivider } from "@/components/section-divider";
-
+import { Skills } from '@/components/skills';
 export default function Home() {
     return (
         <main className="flex flex-col items-center px-4 overflow-x-hidden">
             <Intro />
             <SectionDivider />
             <Experience />
-            <SectionDivider />
+            <Skills/>
             <About />
             <Contact/>
         </main>
