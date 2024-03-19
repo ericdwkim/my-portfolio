804a6a747ea799c9ee7cfe39a5ed6cc8f579fc7c
ericdwkim <ericdwkim.io@gmail.com>
2024-03-19 16:32:34 -0500

    add: challenge me chess hyperlink

--- components/about.tsx
+++ components/about.tsx
@@ -2,7 +2,7 @@
 'use client';
 
 import React from 'react';
-import { SectionHeading } from './section-heading'
+import { SectionHeading } from "@/components/section-heading";
 import { motion } from 'framer-motion';
 import { useSectionInView } from "@/lib/hooks";
 export const About = () => {
@@ -22,7 +22,7 @@ export const About = () => {
             <p>
                 I'm a data analyst and developer based in Texas with strong interests in distributed systems and decentralization.
                 Outside of work, you can find me rock climbing 🧗, training Brazilian jiu-jitsu 🥋, or
-                immersed in a book 📖. I'm also avid chess player, so click ♟️ if you'd like to play a game.
+                immersed in a book 📖. I'm also avid chess player, so click <a href="http://chess.com/play/eek_kim">♘</a> if you wish to challenge me.
                 I have aspirations on becoming a full-stack software architect, so please connect to follow me on my journey!
             </p>
             <br />
