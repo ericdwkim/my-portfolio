67ff29f1823e8307f817e1beb23c6c6f67be2840
ericdwkim <ericdwkim.io@gmail.com>
2024-03-19 16:58:53 -0500

    minor syntax formatting

--- components/skills.tsx
+++ components/skills.tsx
@@ -32,20 +32,20 @@ export const Skills = () => {
         >
             <SectionHeading>My skills </SectionHeading>
             <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
-                {skillsData.map((skill, index ) =>(
-                 <motion.li
-                    className="bg-white border-black rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
-                    key={index}
-                    variants={fadeInAnimationVariants}
-                    initial="initial"
-                    whileInView="animate"
-                    viewport={{
-                        once: true,
-                    }}
-                    custom={index}
-                 >
-                     {skill}
-                 </motion.li>
+                {skillsData.map((skill, index) => (
+                    <motion.li
+                        className="bg-white border-black rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+                        key={index}
+                        variants={fadeInAnimationVariants}
+                        initial="initial"
+                        whileInView="animate"
+                        viewport={{
+                            once: true,
+                        }}
+                        custom={index}
+                    >
+                        {skill}
+                    </motion.li>
                 ))}
             </ul>
             <li/>
