f748589e4b9b28a8aceb6bfb87db74447dd47552
ericdwkim <ericdwkim.io@gmail.com>
2024-04-09 14:54:52 -0500

    wip: `Project` component

--- components/project.tsx
+++ components/project.tsx
@@ -1,3 +1,26 @@
-// 'use client';
-//
-// import { useRef }
+'use client';
+
+import { useRef } from 'react';
+import { projectsData } from "@/lib/data";
+import Image from 'next/image';
+import { motion, useScroll, useTransform } from "framer-motion";
+import Link from 'next/link';
+import { ProjectBtn } from './project-btn';
+
+type ProjectProps = (typeof projectsData)[number];
+
+export const Project = ({
+    title,
+    description,
+    github,
+    tags
+}: ProjectProps) => {
+    const ref = useRef<HTMLDivElement>(null)
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ['0 1', '1.33 1'],
+    });
+
+
+
+};
