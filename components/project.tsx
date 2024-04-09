570d43a7565802bbb8f3de24ed3ae59071dee2fd
ericdwkim <ericdwkim.io@gmail.com>
2024-04-09 16:59:05 -0500

    add: `Project` return barebone jsx object

--- components/project.tsx
+++ components/project.tsx
@@ -2,7 +2,6 @@
 
 import { useRef } from 'react';
 import { projectsData } from "@/lib/data";
-import Image from 'next/image';
 import { motion, useScroll, useTransform } from "framer-motion";
 import Link from 'next/link';
 import { ProjectBtn } from './project-btn';
@@ -20,6 +19,41 @@ export const Project = ({
         target: ref,
         offset: ['0 1', '1.33 1'],
     });
+
+    return (
+        <motion.div
+            ref={ref}
+            className="group mb-3 sm:mb-8 last:mb-0"
+        >
+            <section>
+                <div>
+                    <Link href={github} target="_">
+                        <h3>{title}</h3>
+                    </Link>
+                    <p>
+                        <span className="font-bold underline">Description</span>{' '}{description}
+                    </p>
+
+                    <div>
+                        <a href={github} target="_">
+                            <ProjectBtn/>
+                        </a>
+                    </div>
+
+                    <ul>
+                        {tags.map((tag, index) => (
+                            <li key={index}>
+                                {tag}
+                            </li>
+                        ))}
+                    </ul>
+
+                </div>
+
+            </section>
+
+        </motion.div>
+    )
 
 
 
