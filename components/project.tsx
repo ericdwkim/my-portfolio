1cbe5978a7a5f9783e2bf75be30e90a1d5d2f05c
ericdwkim <ericdwkim.io@gmail.com>
2024-04-18 11:33:59 -0500

    remove: unused `scrollYProgress`

--- components/project.tsx
+++ components/project.tsx
@@ -17,10 +17,6 @@ export const Project = ({
     imageUrl
 }: ProjectProps) => {
     const ref = useRef<HTMLDivElement>(null)
-    const { scrollYProgress } = useScroll({
-        target: ref,
-        offset: ['0 1', '1.33 1'],
-    });
 
     return (
         <motion.div
